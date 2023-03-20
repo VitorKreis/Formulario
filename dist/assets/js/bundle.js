890505b6550587f1a780c8480879cfaa5cecc914
VitorKreis <101025640+VitorKreis@users.noreply.github.com>
2023-03-20 18:44:50 -0300

    first commit

---
+++ dist/assets/js/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log("Hello world" + 474);
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
